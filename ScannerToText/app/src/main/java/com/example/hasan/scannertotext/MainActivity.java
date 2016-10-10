5befb77c3f3b70fe28e14513e7ece7b21ba6f6a0
Huỳnh Hà <hahpse04181@fpt.edu.vn>
2016-10-11 01:11:02 +0700

    Update Gallery

--- ScannerToText/app/src/main/java/com/example/hasan/scannertotext/MainActivity.java
+++ ScannerToText/app/src/main/java/com/example/hasan/scannertotext/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.hasan.scannertotext;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.opengl.EGLExt;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,12 +14,17 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
+    ImageView imgGallery;
+    ImageView View;
+    private static int RESULT_LOAD_IMAGE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
         imageView = (ImageView) findViewById(R.id.infor);
+        imgGallery = (ImageView) findViewById(R.id.galerry);
+        View = (ImageView) findViewById(R.id.view);
         addListener();
     }
 
@@ -26,5 +36,27 @@ public class MainActivity extends AppCompatActivity {
                startActivity(intent);
             }
         });
+        imgGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i,RESULT_LOAD_IMAGE);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            View.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
     }
 }
